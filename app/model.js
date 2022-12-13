916e8e7432e6f1d24d456486126c115c33da4c77
garizola <99741841+garizola@users.noreply.github.com>
2022-12-12 19:38:45 -0500

    .

--- app/model.js
+++ app/model.js
@@ -1,11 +1,65 @@
 var userInfo = {};
 
-var recipeList = [];
+var recipes = [
+  {
+    rImage: "recipe-pizza.jpg",
+    rName: "Supreme Pizza",
+    rDescription: "Make pizza night super duper out of this world with homemade pizza. This recipe is supreme with vegetables and two types of meat. Yum!",
+    rTime: "1hr 24min",
+    rServingSize: "4 servings",
+    ingredients: [
+      "1/4 batch pizza dough",
+      "2 tablespoons Last-Minute Pizza Sauce",
+      "10 slices pepperoni",
+      "1 cup cooked and crumbled Italian sausage",
+      "2 large mushrooms, sliced",
+      "1/4 bell pepper, sliced",
+      "1 tablespoon sliced black olives",
+      "1 cup shredded mozzarella cheese",
+    ],
+    instructions: [
+      "1. Preheat the oven to 475°. Spray pizza pan with nonstick cooking or line a baking sheet with parchment paper.",
+      "2. Flatten dough into a thin round and place on the pizza pan.",
+      "3. Spread pizza sauce over the dough.",
+      "4. Layer the toppings over the dough in the order listed .",
+      "5. Bake for 8 to 10 minutes or until the crust is crisp and the cheese melted and lightly browned.",
+    ],
+    rId: 0,
+  }
+];
+
+var currRecipe = {
+
+    rImage: "recipe-pizza.jpg",
+    rName: "Supreme Pizza",
+    rDescription: "Make pizza night super duper out of this world with homemade pizza. This recipe is supreme with vegetables and two types of meat. Yum!",
+    rTime: "1hr 24min",
+    rServingSize: "4 servings",
+    ingredients: [
+      "1/4 batch pizza dough",
+      "2 tablespoons Last-Minute Pizza Sauce",
+      "10 slices pepperoni",
+      "1 cup cooked and crumbled Italian sausage",
+      "2 large mushrooms, sliced",
+      "1/4 bell pepper, sliced",
+      "1 tablespoon sliced black olives",
+      "1 cup shredded mozzarella cheese",
+    ],
+    instructions: [
+      "1. Preheat the oven to 475°. Spray pizza pan with nonstick cooking or line a baking sheet with parchment paper.",
+      "2. Flatten dough into a thin round and place on the pizza pan.",
+      "3. Spread pizza sauce over the dough.",
+      "4. Layer the toppings over the dough in the order listed .",
+      "5. Bake for 8 to 10 minutes or until the crust is crisp and the cheese melted and lightly browned.",
+    ],
+    rId: 0,
+  }
+
 
 
 
 export function changePage(pageID, callback) {
-  if (pageID == "") {
+  if (pageID == "" || pageID == "home") {
     $.get(`pages/home.html`, function (data) {
      
       $("#app").html(data);
@@ -15,61 +69,31 @@ export function changePage(pageID, callback) {
     $.get(`pages/${pageID}.html`, function (data) {
      
       $("#app").html(data);
-     $("#editRecipe").append(`<div id="create-title">Hey, create recipe!</div>
 
-     <div id="addRecipe-container">
- 
-         <div class="recipe-section">
- 
-             <input type="text" id="rImage" name="rImage" placeholder="Add Recipe Image">
- 
-             <input type="text" id="rName" name="rName" placeholder="Recipe Name">
- 
-             <input type="text" id="rDesc" name="rDesc" placeholder="Recipe Description">
- 
-             <input type="text" id="rTT" name="rTT" placeholder="Recipe Total Time">
- 
-             <input type="text" id="rSS" name="rSS" placeholder="Recipe Serving Size">
-         </div>
- 
-         <div class="enter-section">
-             <h3>Enter Ingredients</h3>
-             <div class="recipe-section">
- 
-                 <input type="text" id="ingre0" placeholder="Ingredient #1"/>
-                 <input type="text" id="ingre1" placeholder="Ingredient #2"/>
-                 <input type="text" id="ingre2" placeholder="Ingredient #3"/>
-      
- 
-                 <div class="addBtn">+</div>
- 
-             </div>
- 
-         </div>
- 
-         <div class="enter-section">
-             <h3>Enter Ingredients</h3>
- 
-             <div class="recipe-section">
- 
- 
-                 <input type="text" id="instruction0" placeholder="Instruction #1"/>
-                 <input type="text" id="instruction1" placeholder="Instruction #2"/>
-                 <input type="text" id="instuction2" placeholder="Instruction #3"/>
-      
- 
-                 <div class="addBtn">+</div>
-                 
-             </div>
- 
-         </div>
- 
-         <button id="final-submit">
-             Create Recipe
-         </button>
- 
-     </div>
- `)
+        $.each(currRecipe, (idx, recipe) => {
+          $(".recipe-section-ing").append(`<input type="text" id="${idx}" placeholder="${recipe.ingredients}"/>`)
+        });
+
+        $.each(currRecipe, (idx, recipe) => {
+          $(".recipe-section-ins").append(`<input type="text" id="${idx}" placeholder="${recipe.instructions}"/>`)
+        })
+
+
+        $.each(currRecipe, (idx, recipe) => {
+          $(".recipe-section").append(`
+          <img src="../images/${recipe.rImage}" alt="${book.bookTitle}" />
+  
+            <input type="text" id="rName" name="rName" placeholder="${recipe.rName}">
+
+            <input type="text" id="rDesc" name="rDesc" placeholder="${recipe.rDescription}">
+
+            <input type="text" id="rTT" name="rTT" placeholder="${recipe.rTime}">
+
+            <input type="text" id="rSS" name="rSS" placeholder="${recipe.rServingSize}">
+          `)
+        })
+
+     
 
       callback();
     });
@@ -78,6 +102,13 @@ export function changePage(pageID, callback) {
       
       $("#app").html(data);
       
+    });
+  
+  } else if (pageID == "yourRecipes"){
+    $.get(`pages/${pageID}.html`, function (data) {
+      
+      $("#app").html(data);
+      //this is not styled correctly
     });
   
   } else {
